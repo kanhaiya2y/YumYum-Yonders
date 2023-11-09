76d3c4234bc7f1fd0d7518a2c71ee1402aa0acaf
kanhaiya2y <93575895+kanhaiya2y@users.noreply.github.com>
2023-11-09 22:11:45 +0530

    made folder structure

---
+++ app/components/Notification.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return ( 
+    <div class="bg-red-500 text-center flex items-center justify-center px-1 py-1 ">Free delivary for all order 20$.order now!</div>
+  )
+}
+
+export default Notification
